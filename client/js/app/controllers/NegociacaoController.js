80b1d1371e4c92e795e0a6553d62834b6a852b67
thiagoarce <thiagoarce@outlook.com>
2020-01-21 21:40:21 -0400

    solve importações duplicadas - indexOf n funciona

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -44,59 +44,63 @@ class NegociacaoController {
                 this._clearForm();
             })
             .catch(erro => {
-        this._mensagem.texto = erro;
-    })
-}
-
-importaNegociacoes() {
-
-    let service = new NegociacaoService();
-    service
-        .obterNegociacoes()
-        .then(negociacoes => {
-            negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
-            this._mensagem.texto = 'Negociações do período importadas com sucesso';
-        })
-        .catch(erro => this._mensagem.texto = erro);
-}
-
-
-apaga() {
-
-    ConnectionFactory
-        .getConnection()
-        .then(connection => new NegociacaoDao(connection))
-        .then(dao => dao.apagaTodos())
-        .then(mensagem => {
-            this._mensagem.texto = mensagem;
-            this._listaNegociacoes.esvazia();
-        })
-        .catch(erro => this._mensagem.texto = erro);
-}
-
-
-_criaNegociacao() {
-    return new Negociacao(
-        DateHelper.textoParaData(this._inputData.value),
-        parseInt(this._inputQuantidade.value),
-        parseFloat(this._inputValor.value)
-    )
-}
-
-
-_clearForm() {
-    this._inputData.value = "";
-    this._inputQuantidade.value = 1;
-    this._inputValor.value = 0.0;
-    this._inputData.focus();
-}
-
-ordena(coluna) {
-    if (this._ordemAtual == coluna) {
-        this._listaNegociacoes.inverteOrdem();
-    } else {
-        this._listaNegociacoes.ordena((a, b) => a[coluna] - b[coluna]);
+                this._mensagem.texto = erro;
+            })
     }
-    this._ordemAtual = coluna;
-}
+
+    importaNegociacoes() {
+
+        let service = new NegociacaoService();
+        service
+            .obterNegociacoes()
+            .then(negociacoes =>
+                negociacoes.filter(negociacao => 
+                    this._listaNegociacoes.negociacoes.indexOf(Negociacao) == -1)
+            )
+            .then(negociacoes => {
+                negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                this._mensagem.texto = 'Negociações do período importadas com sucesso';
+            })
+            .catch(erro => this._mensagem.texto = erro);
+    }
+
+
+    apaga() {
+
+        ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.apagaTodos())
+            .then(mensagem => {
+                this._mensagem.texto = mensagem;
+                this._listaNegociacoes.esvazia();
+            })
+            .catch(erro => this._mensagem.texto = erro);
+    }
+
+
+    _criaNegociacao() {
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        )
+    }
+
+
+    _clearForm() {
+        this._inputData.value = "";
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
+    }
+
+    ordena(coluna) {
+        if (this._ordemAtual == coluna) {
+            this._listaNegociacoes.inverteOrdem();
+        } else {
+            this._listaNegociacoes.ordena((a, b) => a[coluna] - b[coluna]);
+        }
+        this._ordemAtual = coluna;
+    }
 }
