d452a7a77cdfae8824e6e4f6dbcb4ef9de5c7729
thiagoarce <thiagoarce@outlook.com>
2020-01-22 22:57:49 -0400

    polyfill para fetch API

---
+++ client/js/app/polyfill/es6.js
@@ -0,0 +1,6 @@
+if(!Array.prototype.includes){
+    console.log('Polyfill para Array.include aplicado.');
+    Array.prototype.includes = function(elemento){
+        return this.indexOf(elemento) != -1;
+    }
+}
